dddd292c4af4fa32b823370f716d28e4621c33ea
tahsinAmin <muhammadtahsinamin@gmail.com>
2025-09-11 09:20:37 +0600

    feat: [Live Searchbar] conditionally rendering search results

--- app/components/LiveSearch.tsx
+++ app/components/LiveSearch.tsx
@@ -3,13 +3,15 @@
 import React, { useEffect, useRef, useState } from 'react'
 
 interface Props<T> {
-    results: T[];
+    results?: T[];
     renderItem: (item: T) => React.ReactNode;
+    onChange?: React.ChangeEventHandler;
 }
 
-const LiveSearch = ({ results, renderItem }: Props<any>) => {
+const LiveSearch = ({ results = [], renderItem, onChange }: Props<any>) => {
     const [focusIndex, setFocusIndex] = useState<number>(-1);
     const resultContainer = useRef<HTMLDivElement>(null);
+    const [showResults, setShowResults] = useState(false);
 
     const handleKeyDown = (e: React.KeyboardEvent) => {
         const { key } = e;
@@ -36,30 +38,39 @@ const LiveSearch = ({ results, renderItem }: Props<any>) => {
 
         resultContainer.current.scrollIntoView({ block: 'center', inline: 'center' });
     }, [focusIndex]);
+
+    useEffect(() => {
+        if (!results.length) return setShowResults(false);
+        setShowResults(true);
+    }, [results]);
+
     return (
         <div className='h-[90vh] flex items-center justify-center'>
             <div tabIndex={1} onKeyDown={handleKeyDown} className='relative'>
                 <input
+                    onChange={onChange}
                     type="text"
                     placeholder="Search..."
                     className='w-[600px] px-5 py-3 text-lg rounded-full border-2 border-gray-500 focus:border-gray-700 outline-none transition' />
 
                 {/* Search results container */}
-                <div className='absolute mt-1 w-full p-2 bg-white shadow-lg 
+                {showResults && (
+                    <div className='absolute mt-1 w-full p-2 bg-white shadow-lg 
                 rounded-bl rounded-br max-h-56 overflow-y-auto'>
-                    {results.map((item, index) => (
-                        <div
-                            key={index}
-                            ref={index === focusIndex ? resultContainer : null}
-                            style={{
-                                backgroundColor:
-                                    index === focusIndex ? 'rgba(0,0,0,0.1)' : ''
-                            }}
-                            className='cursor-pointer hover:bg-black hover:bg-opacity-10 p-2'>
-                            {renderItem(item)}
-                        </div>
-                    ))}
-                </div>
+                        {results.map((item, index) => (
+                            <div
+                                key={index}
+                                ref={index === focusIndex ? resultContainer : null}
+                                style={{
+                                    backgroundColor:
+                                        index === focusIndex ? 'rgba(0,0,0,0.1)' : ''
+                                }}
+                                className='cursor-pointer hover:bg-black hover:bg-opacity-10 p-2'>
+                                {renderItem(item)}
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
         </div>
     )
--- app/dropdown/page.tsx
+++ app/dropdown/page.tsx
@@ -1,27 +1,38 @@
 'use client';
-
-import React from 'react'
+import React, { useState } from 'react'
 import LiveSearch from '../components/LiveSearch'
 
 interface Props { }
-
 const profiles = [
-    { id: 1, name: 'John Doe' },
-    { id: 2, name: 'Jane Doe' },
-    { id: 3, name: 'John Smith' },
-    { id: 4, name: 'Jane Smith' },
-    { id: 5, name: 'Hannah' },
-    { id: 6, name: 'Heather' },
-    { id: 7, name: 'Helen' },
-    { id: 8, name: 'Gabriele' },
-    { id: 9, name: 'Anne Teak' },
-    { id: 10, name: 'Addie Minstra' },
-    { id: 11, name: 'Anne Ortha' },
+    { id: '1', name: 'John Doe' },
+    { id: '2', name: 'Jane Doe' },
+    { id: '3', name: 'John Smith' },
+    { id: '4', name: 'Jane Smith' },
+    { id: '5', name: 'Hannah' },
+    { id: '6', name: 'Heather' },
+    { id: '7', name: 'Helen' },
+    { id: '8', name: 'Gabriele' },
+    { id: '9', name: 'Anne Teak' },
+    { id: '10', name: 'Addie Minstra' },
+    { id: '11', name: 'Anne Ortha' },
 ]
 
 const page = () => {
+
+    const [results, setResults] = useState<{ id: string, name: string }[]>();
+
+    type changeHandler = React.ChangeEventHandler<HTMLInputElement>;
+    const handleChange: changeHandler = (e) => {
+        const { target } = e;
+
+        if (!target.value.trim()) return setResults([]);
+
+        const filteredResults = profiles.filter((profile) =>
+            profile.name.toLowerCase().includes(target.value.toLowerCase()));
+        setResults(filteredResults);
+    }
     return (
-        <LiveSearch results={profiles} renderItem={(item) => <p>{item.name}</p>} />
+        <LiveSearch results={results} onChange={handleChange} renderItem={(item) => <p>{item.name}</p>} />
     )
 }
 
